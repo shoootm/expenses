b4ed02670d2a108efa540b9fe8b86c2288db086e
shoootm <73105615+shoootm@users.noreply.github.com>
2020-11-26 11:26:47 +0900

    ユーザー管理機能実装

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  with_options presence: true do
+    validates :nickname
+    validates :prefecture
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe User do
+    before do
+      @user = FactoryBot.build(:user)
+    end
+
+    describe 'ユーザー新規登録' do
+      context '新規登録がうまくいくとき' do
+        it "全てが正常である" do
+          expect(@user).to be_valid
+        end
+        it "municipalitieが空でも登録できること" do
+          @user.municipalitie = nil
+          expect(@user).to be_valid
+        end
+      end
+
+      context '新規登録がうまくいかないとき' do
+        it "nicknameが空だと登録できない" do
+          @user.nickname = nil
+          @user.valid?
+          expect(@user.errors.full_messages).to include("Nickname can't be blank")
+        end
+        it "emailが空では登録できない" do
+          @user.email = nil
+          @user.valid?
+          expect(@user.errors.full_messages).to include("Email can't be blank")
+        end
+        it "重複したemailが存在する場合登録できない" do
+          @user.save
+          another_user = FactoryBot.build(:user)
+          another_user.email = @user.email
+          another_user.valid?
+          expect(another_user.errors.full_messages).to include("Email has already been taken")
+        end
+        it "emailは@が含まれてなければ登録できない" do
+          @user.email = "testsample.com"
+          @user.valid?
+          expect(@user.errors.full_messages).to include("Email is invalid")
+        end
+        it "passwordが空では登録できない" do
+          @user.password = nil
+          @user.valid?
+          expect(@user.errors.full_messages).to include("Password can't be blank")
+        end
+        it "passwordが5文字以下であれば登録できない" do
+          @user.password = '111qa'
+          @user.valid?
+          expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+        end
+        it "passwordとpassword_confirmationが一致しなければ登録できない" do
+          @user.password = '111qaz'
+          @user.valid?
+          expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+        end
+        it "prefectureが空では登録できない" do
+          @user.prefecture = nil
+          @user.valid?
+          expect(@user.errors.full_messages).to include("Prefecture can't be blank")
+        end
+      end
+    end
+  end
+end
