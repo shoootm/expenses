1a5f96fd1d9662684d6370c5ebe0132f0a9d2cc0
shoootm <73105615+shoootm@users.noreply.github.com>
2020-11-27 12:40:01 +0900

    家計簿作成機能実装

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,4 +1,17 @@
 class ExpensesController < ApplicationController
   def index
+    @expenses = Expense.all
+  end
+  def new
+    @expense = Expense.new
+  end
+  def create
+    @expense = Expense.new(expense_params)
+    @expense.save
+    redirect_to action: :index
+  end
+  private
+  def expense_params
+    params.require(:expense).permit(:income, :food_expense, :necessities, :clothes_expense, :beauty_expense, :entertainment_expense, :medical_bill, :education_cost, :utility_costs, :traveling_expense, :other, :memo).merge(user_id: current_user.id)
   end
 end
