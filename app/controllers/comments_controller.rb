d181685697e69e39e5f63e715e34d4ea4185a3b7
shoootm <73105615+shoootm@users.noreply.github.com>
2020-11-30 16:40:27 +0900

    コメント削除機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,18 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_expense, only: [:create ,:destroy]
   def create
-    @expense = Expense.find(params[:expense_id])
     @comment = Comment.new(comment_params)
     if @comment.save
+      redirect_to expense_path(@expense.id)
+    else
+      render expense_path(@expense.id)
+    end
+  end
+  def destroy
+    @comment = Comment.find(params[:id])
+    if current_user.id == @comment.user_id
+      @comment.destroy
       redirect_to expense_path(@expense.id)
     else
       render expense_path(@expense.id)
@@ -14,5 +23,8 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, expense_id: params[:expense_id])
+  end
+  def set_expense
+    @expense = Expense.find(params[:expense_id])
   end
 end
