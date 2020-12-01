d42e66f3548bde5040b566f785b470cb7f7777dc
shoootm <73105615+shoootm@users.noreply.github.com>
2020-12-01 15:01:18 +0900

    家計簿一覧表示機能実装中止

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   def show
-    @expenses = current_user.expenses
+    if user_signed_in? || current_user.id == params[:user_id]
+      @expenses = current_user.expenses
+    else
+      render root_path
+    end
   end
 end
